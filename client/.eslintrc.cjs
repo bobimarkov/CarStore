828d518f0845a5e2bc2c8e0e4e069bab5288207f
Boris Markov <bomar3110@gmail.com>
2023-04-26 16:09:25 +0300

    React initialization

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    "plugin:react-hooks/recommended",
+    "react-app",
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  overrides: [
+  ],
+  ignorePatterns: ['dist/**/*.js', '*.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json']
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/restrict-template-expressions": "off"
+  }
+}
